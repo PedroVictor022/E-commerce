c6f826b3b7597fbf3b2b4d87f61fdf46d7080966
Pedro Victor <pedro20002victor@gmail.com>
2022-09-04 10:20:12 -0300

    Add content page cart

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -1,7 +1,28 @@
+import { useContext } from "react"
+import { CartContext } from "../context/CartContext"
+import { ProductItem } from "../styles/CartStyle"
+
 export const CartPage = () => {
+   const { cart } = useContext(CartContext)
    return (
       <div>
          <h1>Seu carrinho</h1>
+         {
+            cart.map(item => {
+               return (
+                  <ProductItem
+                     key={item.id}
+                  >
+                     <div className="flexStart">
+                        <img src={item.img} alt="" />
+                        <h2>{item.name}</h2>
+                        <p>de R${item.price}</p>
+                     </div>
+                     <button>Comprar</button>
+                  </ProductItem>
+               )
+            })
+         }
       </div>
    )
 }
