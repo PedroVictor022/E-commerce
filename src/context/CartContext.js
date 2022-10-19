b855907db19b562083b629a8153b38d4394b03bb
Pedro Victor <pedro20002victor@gmail.com>
2022-10-19 10:10:24 -0300

    New imagens add

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -6,18 +6,18 @@ const CartProvider = ({ children }) => {
    const [cart, setCart] = useState([]);
    const [cartTotal, setCartTotal] = useState([]);
 
-   const products1 = [
-      {
-         id: Math.random(Math.random() * 100).toFixed(5),
-         name: "Teste 1",
-         img: "teste",
-         price: 100000,
-      }
-   ]
+   // const products1 = [
+   //    {
+   //       id: Math.random(Math.random() * 100).toFixed(5),
+   //       name: "Teste 1",
+   //       img: "teste",
+   //       price: 100000,
+   //    }
+   // ]
    
    return (
       <CartContext.Provider
-         value={{ cart, setCart, products1 }}
+         value={{ cart, setCart }}
       >
          {children}
       </CartContext.Provider>
